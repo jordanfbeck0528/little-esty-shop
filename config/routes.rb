e969205ad7a3fb67d5d39c1fdf0ca3ab5e781419
Brisa Garcia <brisagarciaglz@gmail.com>
2021-02-24 14:59:41 -0700

    WIP for merchant controller folders

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  scope module: 'merchant' do
   resources :merchants do
     resources :dashboard, only: [:index]
-    resources :items
-    resources :invoices
+    resources :items, only: [:index]
+    resources :invoices, only: [:index]
+    end
   end
 end
--- spec/features/merchant/index_spec.rb
+++ spec/features/merchant/index_spec.rb
@@ -8,5 +8,15 @@ RSpec.describe 'As a Merchant', type: :feature do
       visit merchant_dashboard_index_path(@merchant)
       expect(page).to have_content(@merchant.name)
     end
+
+    # it 'Then I see link to my merchant items index (/merchant/merchant_id/items))' do
+    #   visit merchant_dashboard_index_path(@merchant)
+    #
+    #   expect(page).to have_content('Merchant Items Index')
+    #   expect(page).to have_link(href: merchant_items_path(@merchant))
+    #
+    #   expect(page).to have_content('Merchant Invoices Index')
+    #   expect(page).to have_link(href: merchant_invoices_path(@merchant))
+    # end
   end
 end
