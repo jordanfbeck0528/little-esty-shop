12441169275fd8a8fccd85a9254881de4cf95183
Brisa Garcia <brisagarciaglz@gmail.com>
2021-02-27 19:10:53 -0700

    Add update to controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,4 +12,28 @@ def index
   def edit
     @item = Item.find(params[:id])
   end
+
+  def update
+    @item = Item.find(params[:id])
+    if params[:update_status] == 'Disabled'
+      @item.update!(status: 1)
+      flash[:success] = "Item #{@item.name} Disabled"
+      redirect_to merchant_items_path(@item.merchant.id)
+    elsif params[:update_status] == 'Enabled'
+      @item.update!(status: 0)
+      flash[:success] = "Item #{@item.name} Enabled"
+      redirect_to merchant_items_path(@item.merchant.id)
+    else
+      @item.update!(item_params)
+      flash[:success] = 'Item successfully updated'
+      render :show
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price)
+  end
+
 end
