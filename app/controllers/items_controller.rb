61a016b0d5d114f1ea3d7b2cfb4d3e7826b9489a
Brisa Garcia <brisagarciaglz@gmail.com>
2021-02-28 11:07:26 -0700

    Add methods for item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-def index
+  def index
     merchant = Merchant.find(params[:merchant_id])
     @merchant_items = merchant.items
   end
@@ -8,4 +8,32 @@ def index
     merchant = Merchant.find(params[:merchant_id])
     @item = merchant.items.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if params[:update_status] == 'Disabled'
+      @item.update!(status: 1)
+      flash[:success] = "Item #{@item.name} Disabled"
+      redirect_to merchant_items_path(@item.merchant.id)
+    elsif params[:update_status] == 'Enabled'
+      @item.update!(status: 0)
+      flash[:success] = "Item #{@item.name} Enabled"
+      redirect_to merchant_items_path(@item.merchant.id)
+    else
+      @item.update!(item_params)
+      flash[:success] = 'Item successfully updated'
+      render :show
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price)
+  end
+
 end
